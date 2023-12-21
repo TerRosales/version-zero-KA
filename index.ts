162613c6fba91034536901dac6e587ad8fabdd81
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2023-12-21 16:59:14 -0500

    design data models

---
+++ index.ts
@@ -0,0 +1,49 @@
+// interface ParentUser {
+//   username: string;
+//   emailAddress: string;
+//   securityPin: number;
+//   password: string;
+//   children: ChildUser[];
+// }
+
+// interface GuardianUser {
+//   username: string;
+//   emailAddress: string;
+//   securityPin: number;
+//   password: string;
+//   children: ChildUser[];
+//   listOfChores: Chore[];
+// }
+
+// interface ChildUser {
+//   username: string;
+//   emailAddress: string;
+//   securityPin: number;
+//   password: string;
+//   listOfChores: Chore[];
+// }
+
+// interface Calendar {
+//   days: number;
+// }
+
+// interface Days {
+//   calendarDays: number;
+//   listOfChores: Chores[];
+// }
+
+// interface Chore {
+//   name: string;
+//   description: string;
+//   reward: number;
+//   isCompleted: boolean;
+//   deadline: string;
+// }
+
+// //TODO
+// // Badges
+// // Credit info
+
+// interface LeaderBoard {
+//   listChildren: ChildUser[];
+// }
